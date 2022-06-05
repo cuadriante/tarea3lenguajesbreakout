e00cbcd6a8c5028bcdeee21732073342c192f468
cuadriante <adriana@sinapsis.ws>
2022-06-04 21:53:42 -0600

    labels

--- src/main/java/org/breakout/GameWindow.java
+++ src/main/java/org/breakout/GameWindow.java
@@ -3,8 +3,10 @@ package org.breakout;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.breakout.blockFactory.Block;
 import org.breakout.blockFactory.BlockFactory;
@@ -17,6 +19,19 @@ public class GameWindow {
     final int STAGE_WIDTH = 400;
     final int STAGE_HEIGHT = 400;
     private Pane root;
+    public static int pts = 0;
+    public static int lvl = 0;
+
+    public static int life = 3;
+
+    private static final Text puntosLabel = new Text();
+    private static final Text levelLabel = new Text();
+
+    private static final Text livesLabel = new Text();
+    private static final Text puntos = new Text();
+    private static final Text level = new Text();
+
+    private static final Text lives = new Text();
     private final PlayerBar playerBar = new PlayerBar(200, 350, BlockFactory.getWidth(), BlockFactory.getHeight());
     // private final Ball ball = new Ball(100, 100, 10);
     private ArrayList<Ball> ballList = new ArrayList<Ball>();
@@ -26,10 +41,69 @@ public class GameWindow {
 
     GameWindow(Stage Lobby) throws Exception {
         Lobby.setTitle("Breakout");
+        Lobby.setResizable(false);
         Ball ball = new Ball(STAGE_WIDTH/2, STAGE_HEIGHT - 180, this);
         ballList.add(ball);
         start(Lobby);
+        createLabels();
         connectToClient();
+    }
+
+    private void createLabels() {
+
+        double fontSize = 15;
+        FontWeight fontWeight = FontWeight.BOLD;
+        Font numberFont = Font.font("Biome", fontWeight,fontSize);
+
+        double labelFontSize = 10;
+        FontWeight labelFontWeight = FontWeight.BOLD;
+        Font labelFont = Font.font("Biome", labelFontWeight,labelFontSize);
+
+        String puntaje = Integer.toString(pts);
+        puntos.setText(puntaje);
+        puntos.setX(10);
+        puntos.setY(30);
+        puntos.setFill(Color.SADDLEBROWN);
+        puntos.setFont(numberFont);
+
+        puntosLabel.setText("SCORE");
+        puntosLabel.setX(10);
+        puntosLabel.setY(15);
+        puntosLabel.setFill(Color.SADDLEBROWN);
+        puntosLabel.setFont(labelFont);
+
+        String nivelString = Integer.toString(lvl);
+        level.setText(nivelString);
+        level.setX(100);
+        level.setY(30);
+        level.setFill(Color.SADDLEBROWN);
+        level.setFont(numberFont);
+
+        levelLabel.setText("LEVEL");
+        levelLabel.setX(100);
+        levelLabel.setY(15);
+        levelLabel.setFill(Color.SADDLEBROWN);
+        levelLabel.setFont(labelFont);
+
+        String livesString = Integer.toString(life);
+        lives.setText(livesString);
+        lives.setX(380);
+        lives.setY(30);
+        lives.setFill(Color.SADDLEBROWN);
+        lives.setFont(numberFont);
+
+        livesLabel.setText("LIVES");
+        livesLabel.setX(365);
+        livesLabel.setY(15);
+        livesLabel.setFill(Color.SADDLEBROWN);
+        livesLabel.setFont(labelFont);
+
+        root.getChildren().add(puntos);
+        root.getChildren().add(level);
+        root.getChildren().add(lives);
+        root.getChildren().add(livesLabel);
+        root.getChildren().add(puntosLabel);
+        root.getChildren().add(levelLabel);
     }
 
     private void connectToClient() {
